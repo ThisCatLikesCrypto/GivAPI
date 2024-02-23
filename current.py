926bc3809adc10fe4f72ee4048038d04bdbced87
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-02-23 19:51:55 +0000

    Add files via upload

---
+++ current.py
@@ -0,0 +1,25 @@
+import requests
+
+def getData(serial, api_key):
+    url = "https://api.givenergy.cloud/v1/inverter/" + serial + "/system-data/latest"
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}",
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        # Success
+        data = response.json()
+    else:
+        # Error
+        data = {"error": response.status_code}
+    
+    return data['data']
+
+
+#Example use
+if __name__ == "__main__":
+    serial = YOUR_SERIAL_HERE
+    api_key = YOUR_KEY_HERE # (needs api:inverter:read to function)
+    print(getData(serial, api_key))
