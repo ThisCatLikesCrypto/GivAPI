74faa8de0c619e836aee77b0b71dcbc263b908a2
Wilbur Williams <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-03-11 10:00:01 +0000

    Create setup.py

---
+++ GivAPI/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GivAPI',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['requests'],
+    author='Wilbur Williams',
+    description='Gets current data from the GivEnergy API',
+    url='https://github.com/ThisCatLikesCrypto/GivAPI',
+)
